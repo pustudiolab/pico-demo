58782a46faee41702083b694e32c116e461d33ba
Adham Ibrahim <adham.ibrahim@princeton.edu>
2024-11-06 17:50:56 -0500

    rename mario

--- light_level.py
+++ light_level.py
@@ -7,54 +7,46 @@ leds = [7, 6, 5, 4, 3, 2, 1, 0]
 for x in range(len(leds)):
     leds[x] = Pin(leds[x], Pin.OUT)
 
-def binary_display(led_list: list, number_to_display: int):
-    '''
-    Displays a number in binary using the line of onboard LEDs located above the line of buttons.
-    
-    Args:
-        led_list: 8 element list representing the 8 onboard LEDs labelled respectively 1, 2, 4, 8, 16, 32, 64, & 128
-        number_to_display: the number to display in binary on the onboard LEDs
-    '''
-    __binary_display_recur(led_list, number_to_display, 0)
-
-def __binary_display_recur(led_list: list, number_to_display: int, its: int):
-    '''
-    DO NOT TOUCH THIS FUNCTION
-    Recursive function for displaying binary on the onboard LEDs.
-    Use the wrapper function Binary_display.
-    '''
-    #clear
-    for x in range(8):
-        led_list[x].off()
-    if number_to_display >= 1:
-        __binary_display_recur(led_list, number_to_display // 2, its + 1)
-    if number_to_display % 2 == 1:
-        led_list[7 - its].on()
-    else:
-        led_list[7 - its].off()
-
 speaker = PWM(Pin(15))
 
 buttons = [10, 11, 12, 13, 14]
 for x in range(len(buttons)):
     buttons[x] = Pin(buttons[x], Pin.IN, Pin.PULL_UP)
 
+# These values represent the lowest and highest readings, and
+# the actual reading will be a percentage between these two
 mxsf = 0
 mnsf = 65536
+raw_readings = []
 while True:
-    utime.sleep(0.05)
-    reading = pr.read_u16()
-    freq = max(440.*8*(mxsf-reading)/(abs(mxsf)+1), 130.81)
-    mxsf = (mxsf + max(reading, mxsf)) / 2.
-    mnsf = (mnsf + min(reading, mnsf)) / 2.
-    light_level = round(8.*(mxsf-reading)/(abs(mxsf-mnsf)+1))
+    utime.sleep(0.01)
+    raw_readings.append(pr.read_u16())
+    if len(raw_readings) > 1000:
+        raw_readings = raw_readings[1:]
+    
+    if (len(raw_readings) < 10):
+        continue
+
+    raw_reading = sum(raw_readings[-10:]) / 10
+    mxsf = max(raw_readings)
+    mnsf = min(raw_readings)
+
+    # Value from 0 to 1 
+    value = (mxsf - raw_reading) / (abs(mxsf-mnsf)+1)
+    light_level = round(8 * value)
     if mnsf < mxsf:
-        binary_display(leds,(1<<min(max(light_level,0),8))-1)
+        for led in leds[0:light_level-1]:
+            led.on()
+        for led in leds[light_level:]:
+            led.off()
+
+    # play on the speaker if rightmost button pressed
     if not buttons[0].value():
+        freq = max(440.*8*(mxsf-raw_reading)/(abs(mxsf)+1), 130.81)
         speaker.freq(round(freq))
         speaker.duty_u16(2 ** 15)
     else:
         speaker.duty_u16(0)
-    print(mnsf,reading,mxsf)
+    print(mnsf,raw_reading,mxsf)
     
 speaker.deinit()
---
+++ mario.py
@@ -0,0 +1,63 @@
+from machine import Pin, PWM
+import utime
+
+'''
+['C', 'Cs', 'D', 'Ds', 'E', 'F', 'Fs', 'G', 'Gs', 'A', 'As', 'B']
+  0    1     2    3     4    5    6     7    8     0    10    11
+ 12   13    14   15    16   17   18    19   20    21    22    23
+ 24   25    26   27    28   29   30    31   32    33    34    35
+ 36   37    38   39    40   41   42    43   44    45    46    47
+'''
+
+# Start with A1
+note_frequencies = [130.81]
+note_names = ['C', 'Cs', 'D', 'Ds', 'E', 'F', 'Fs', 'G', 'Gs', 'A', 'As', 'B']
+
+for note in range(1, 49):
+    note_frequencies.append(note_frequencies[0] * (2. ** (float(note) / 12.)))
+    print(str(note_names[note % 12]) + ' ' + str(note_frequencies[-1]))
+
+# Read music csv file
+music = []
+with open('mario_1.txt','r') as file:
+    for line in file:
+        line = line.rstrip('\n')
+        line = line.rstrip('\r')
+        music.append(line.split(','))
+        
+print(music)
+
+print('\n')
+tempo_bpm = music[0][0]
+print('tempo (BPM):           ' + str(tempo_bpm))
+quarter_note_time = 60. / float(tempo_bpm)
+print('Quarter note time (s): ' + str(quarter_note_time))
+print('\n')
+
+speaker = PWM(Pin(15))
+for idx in range(1, len(music)):
+    print(music[idx])
+    if int(music[idx][1]) >= 0:
+        note_duration = 4 * quarter_note_time / float(music[idx][0])
+        note_freq = note_frequencies[int(music[idx][1])]
+        note_name = note_names[int(music[idx][1]) % 12]
+        
+        start_time = utime.ticks_ms()
+        
+        speaker.freq(round(note_freq))
+        speaker.duty_u16(2 ** 15)
+        print('Playing note ' + note_name + ' @' + str(note_freq) + 'Hz, 1 / ' + str(int(music[idx][0])) + ' -th note')
+    else:
+        note_duration = 4 * quarter_note_time / float(music[idx][0])
+        
+        start_time = utime.ticks_ms()
+        
+        speaker.duty_u16(0)
+        
+        print('Rest, 1 / ' + str(int(music[idx][0])) + ' -th note')
+        
+            
+    while utime.ticks_diff(utime.ticks_ms(), start_time) < note_duration * 1000:
+        pass
+    
+speaker.duty_u16(0)
